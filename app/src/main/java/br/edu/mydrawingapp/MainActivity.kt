7529071e1f3333c3d7a310815384d8940a5a7d7a
joaovq <vitorsnta90@gmail.com>
2022-12-20 18:17:36 -0300

    MainActivity.kt

--- app/src/main/java/br/edu/mydrawingapp/MainActivity.kt
+++ app/src/main/java/br/edu/mydrawingapp/MainActivity.kt
@@ -1,14 +1,22 @@
 package br.edu.mydrawingapp
 
 import android.app.Dialog
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.forEach
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var drawingView:DrawingView
     private lateinit var btnBrush:ImageButton
+    private lateinit var mImageButtonCurrentPaint:ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +24,23 @@ class MainActivity : AppCompatActivity() {
         drawingView = findViewById(R.id.drawing_view)
         btnBrush= findViewById(R.id.ib_brush)
 
+        val linearLayoutPaintColors = findViewById<LinearLayout>(R.id.ll_paint_colors)
+
+
+        mImageButtonCurrentPaint = linearLayoutPaintColors[0] as ImageButton
+        mImageButtonCurrentPaint.setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.pallet_pressed
+            )
+        )
+
+        linearLayoutPaintColors.forEach {
+                view ->
+            val imageButton = view as ImageButton
+            imageButton.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.pallet_normal))
+
+        }
 
         drawingView.setSizeForBrush(20F)
         btnBrush.setOnClickListener{
@@ -32,19 +57,21 @@ class MainActivity : AppCompatActivity() {
         val largeBtn = brushDialog.findViewById<ImageButton>(R.id.ib_large_brush)
         smallBtn.setOnClickListener{
             //            TODO: Put the sizes in Enum Class
-            drawingView.setSizeForBrush(10F)
+            drawingView.setSizeForBrush(BrushSize.SMALL.value)
             brushDialog.dismiss()
         }
         mediumBtn.setOnClickListener{
             //            TODO: Put the sizes in Enum Class
-            drawingView.setSizeForBrush(20F)
+            drawingView.setSizeForBrush(BrushSize.MEDIUM.value)
             brushDialog.dismiss()
         }
         largeBtn.setOnClickListener{
 //            TODO: Put the sizes in Enum Class
-            drawingView.setSizeForBrush(30F)
+            drawingView.setSizeForBrush(BrushSize.LARGE.value)
             brushDialog.dismiss()
         }
         brushDialog.show()
     }
+
+
 }
