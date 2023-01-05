1dfa8dd86cbdbafd496575c78c6cd4a3f1fe568b
joaovq <vitorsnta90@gmail.com>
2023-01-05 01:26:09 -0300

    Complete App

--- app/src/main/java/br/edu/mydrawingapp/MainActivity.kt
+++ app/src/main/java/br/edu/mydrawingapp/MainActivity.kt
@@ -4,14 +4,17 @@ import android.Manifest
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.media.MediaScannerConnection
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
+import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
@@ -20,9 +23,11 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
-import androidx.recyclerview.widget.RecyclerView
-import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.CoroutineScope
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity(){
 
@@ -33,7 +38,11 @@ class MainActivity : AppCompatActivity(){
     private lateinit var linearLayoutPaintColors:LinearLayout
     private lateinit var btnGallery:ImageButton
     private lateinit var btnUndo:ImageButton
+    private lateinit var btnSave:ImageButton
+    private lateinit var customProgressDialog: Dialog
+
 //  Com o laucher de gallery, iniciamos uma activity para o result das permissões
+
     private val openGalleryLaucher:ActivityResultLauncher<Intent> = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()){
         result ->
@@ -52,14 +61,14 @@ class MainActivity : AppCompatActivity(){
            val permissionName = permission.key
            val isGranted = permission.value
            if (isGranted){
-               Toast.makeText(
-                   applicationContext,
-                   "Permission granted, now you can read for  storage files",
-                   Toast.LENGTH_SHORT
-               ).show()
+                   Toast.makeText(
+                       applicationContext,
+                       "Permission granted, now you can read for storage files",
+                       Toast.LENGTH_SHORT
+                   ).show()
 //            Intent da ação de colocar e lançando a abertura da galeria do celular
-               val pickIntent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-               openGalleryLaucher.launch(pickIntent)
+                   val pickIntent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                   openGalleryLaucher.launch(pickIntent)
            }else{
                if (permissionName == Manifest.permission.READ_EXTERNAL_STORAGE){
                    Toast.makeText(
@@ -75,12 +84,16 @@ class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        Esconder a ActionBar
+//        supportActionBar?.hide()
+
         drawingView = findViewById(R.id.drawing_view)
         btnBrush= findViewById(R.id.ib_brush)
 
         linearLayoutPaintColors = findViewById(R.id.ll_paint_colors)
         btnGallery = findViewById(R.id.ib_gallery)
         btnUndo = findViewById(R.id.ib_undo)
+        btnSave = findViewById(R.id.ib_save)
 
         mImageButtonCurrentPaint = linearLayoutPaintColors[0] as ImageButton
         mImageButtonCurrentPaint.setImageDrawable(
@@ -106,6 +119,27 @@ class MainActivity : AppCompatActivity(){
             drawingView.onClickUndo()
         }
 
+        btnSave.setOnClickListener{
+            if(isReadStorageAllowed()) {
+                showProgressDialog()
+                val flDrawingView:FrameLayout = findViewById(R.id.fl_drawing_view_container)
+                lifecycleScope.launch{
+                    delay(1000)
+                    val bitmapFromView:Bitmap = getBitmapFromView(flDrawingView)
+                    saveBitmapFile(bitmapFromView)
+                }
+            }
+            else{
+                requestStoragePermission()
+            }
+        }
+    }
+
+    private fun isReadStorageAllowed():Boolean{
+        val result = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        return result == PackageManager.PERMISSION_GRANTED
     }
 
     private fun requestStoragePermission() {
@@ -117,8 +151,61 @@ class MainActivity : AppCompatActivity(){
                 "Put photos in app cannot be used because read external file access is denied"
             )
         } else {
-            requestPermission.launch(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE))
+            requestPermission.launch(arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ))
         }
+    }
+
+    private suspend fun saveBitmapFile(mBitmap:Bitmap?):String{
+        var result = ""
+        withContext(Dispatchers.IO){
+            mBitmap?.let {
+                try {
+                    val bytes = ByteArrayOutputStream()
+                    mBitmap.compress(Bitmap.CompressFormat.PNG, 90, bytes)
+
+//                    instanciando um arquivo com um nome diferente de caminho para cada salvamento
+//                    Estamos utilizando o System.currentTimeMillis() para ser mais aleatório
+//                    File.separator é o separador padrão do sistema, dependente do sistema
+                    val file = File(externalCacheDir?.absoluteFile.toString()
+                            + File.separator + "KidsDrawingApp_"+ System.currentTimeMillis()/1000+ ".png"
+                    )
+                    val outputStream = FileOutputStream(file)
+                    outputStream.write(bytes.toByteArray())
+                    outputStream.close()
+
+                    result = file.absolutePath
+
+                    runOnUiThread{
+                        cancelProgressDialog()
+                        if (result.isNotEmpty()){
+                            val toastCompleteSaveFile = Toast.makeText(
+                                this@MainActivity,
+                                "File saved sucessfully: $result",
+                                Toast.LENGTH_LONG
+                            )
+                            toastCompleteSaveFile.show()
+                            shareImage(result)
+                        }else{
+                            val toastWrongSaveFile = Toast.makeText(
+                                this@MainActivity,
+                                "Something went wrong while saving the file",
+                                Toast.LENGTH_LONG
+                            )
+                            toastWrongSaveFile.show()
+                        }
+//                        Cancelando o dialogo de carregamento
+                    }
+                }
+                catch (e:Exception){
+                    result = ""
+                    e.printStackTrace()
+                }
+            }
+        }
+        return result
     }
 
     //    Images are Bitmap and Bitmap can be storage.
@@ -160,9 +247,25 @@ class MainActivity : AppCompatActivity(){
         alertDialog.create().show()
 
     }
+    private fun showProgressDialog(){
+        customProgressDialog = Dialog(this@MainActivity)
+
+        /*Seta a tela através de um layout resource
+        * O resource layout vai ser inflado, adicionando acima de todas as views da tela*/
+        customProgressDialog.setContentView(R.layout.dialog_custom_progress)
+        customProgressDialog.setCancelable(false)
+//          Inicia o Dialogo e mostra na tela
+        customProgressDialog.show()
+    }
+
+    private fun cancelProgressDialog(){
+        customProgressDialog.dismiss()
+    }
 
     private fun showBrushSizeChooserDialog(){
         val brushDialog = Dialog(this)
+//        /*Seta a tela através de um layout resource
+//        * O resource layout vai ser inflado, adicionando acima de todas as views da tela*/
         brushDialog.setContentView(R.layout.dialog_brush_size)
         brushDialog.setTitle("Brush size:  ")
         val smallBtn = brushDialog.findViewById<ImageButton>(R.id.ib_small_brush)
@@ -188,6 +291,7 @@ class MainActivity : AppCompatActivity(){
         val colorTag: String?
         if(view !== mImageButtonCurrentPaint){
             imageButton = view as ImageButton
+//            Pega a tag pela cor em formato de string
             colorTag = imageButton.tag.toString()
         }else{
             colorTag = view.tag.toString()
@@ -205,4 +309,31 @@ class MainActivity : AppCompatActivity(){
         mImageButtonCurrentPaint = view
     }
 
+    private fun shareImage(result: String){
+//        Provê um caminho para as aplicações
+//        Provê uma interface para scannear a media
+//        Passamos o contexto, o caminho
+        MediaScannerConnection.scanFile(this, arrayOf(result), null,
+            ){
+//            Atraves desse caminho, será escaneado para enviar para o lugar desejado
+            path, uri->
+            /*Um Intent no sistema operacional
+            Android é um mecanismo de software
+            que permite aos usuários coordenar
+             funções de diferentes atividades para realizar uma tarefa.*/
+            val shareIntent = Intent()
+//          A coordenação da ação de mandar e compartilhar com alguém
+            shareIntent.action = Intent.ACTION_SEND
+            shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+            shareIntent.type = "image/png"
+//            iniciando uma atividade de Chooser. Escolher para aonde vai mandar
+
+            /*Convenience function for creating a ACTION_CHOOSER Intent.
+                Builds a new ACTION_CHOOSER Intent that wraps the given target intent,
+                also optionally supplying a title.
+                If the target intent has specified*/
+            startActivity(Intent.createChooser(shareIntent,"Share for friends"))
+        }
+
+    }
 }
